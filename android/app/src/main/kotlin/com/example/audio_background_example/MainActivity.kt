531579834e49ab87dc92632c72efde547577c409
Abdul Rehman <002abdulrehman@gmail.com>
2023-08-05 22:57:59 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/audio_background_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.audio_background_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:audio_background_example/audio_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:just_audio_background/just_audio_background.dart';
+
+void main() async {
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
+    androidNotificationChannelName: 'Audio playback',
+    androidNotificationOngoing: true,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+         
+          primarySwatch: Colors.blue,
+        ),
+        home: AudioScreen() //const MyHomePage(title: 'Flutter Demo Home Page'),
+        );
+  }
+}
